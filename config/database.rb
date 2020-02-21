ecd4a86cd2feafaa731e529f7988645489ebbb8c
Sara Tung <saratung@Saras-MacBook-Pro.local>
2020-02-20 19:04:44 -0800

    prepare for heroku deploy

--- config/database.rb
+++ config/database.rb
@@ -1,29 +1,5 @@
 configure do
-  # Log queries to STDOUT in development
-# A sample Gemfile
-source "https://rubygems.org"
-
-gem 'rake'
-gem 'activesupport'
-
-gem 'sinatra'
-gem 'sinatra-contrib'
-gem 'sinatra-activerecord'
-
-gem 'puma'
-gem 'tux'
-gem 'pry'
-
-group :development, :test do
-  gem 'shotgun'
-  gem 'sqlite3'
-end
-
-group :production do 
-  gem 'rails_12factor'
-  gem 'pg'
-end
-if Sinatra::Application.development?
+  if Sinatra::Application.development?
     set :database, {
       adapter: "sqlite3",
       database: "db/db.sqlite3"
